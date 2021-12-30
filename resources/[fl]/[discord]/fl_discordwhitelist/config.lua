21e899b715c8f815ee928833d906c438df66f6d0
Francis Rivard <frivard93@outlook.com>
2021-12-30 16:39:51 -0500

    Fix Errors for DiscordWhitelist Config

--- resources/[fl]/[discord]/fl_discordwhitelist/config.lua
+++ resources/[fl]/[discord]/fl_discordwhitelist/config.lua
@@ -8,3 +8,35 @@ Config = {
 		["Citoyen(ne)"] = ""
 	}
 }
+
+Config.Points = 0
+
+Config.EmojiList = {}
+
+Config.NoSteam = 'Vous devez ouvrir votre steam pour jouer sur notre serveur.'
+
+Config.Accident = 'Une erreur est survenue.'
+
+Config.Error = 'Une erreur est survenue.'
+
+Config.TimerRefreshClient = 1
+
+Config.TimerCheckPlaces = 1
+
+Config.AddPoints = 1
+
+Config.RemovePoints = 1
+
+Config.EnRoute = ''
+
+Config.PointsRP = ''
+
+Config.Position = 'Position'
+
+Config.EmojiMsg = 'Emoji'
+
+Config.LoterieBonusPoints = ''
+
+Config.PleaseWait_1 = ''
+
+Config.PleaseWait_2 = ''
